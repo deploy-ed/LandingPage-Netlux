b479e593e3f2fb888c81eda099a5ed338c90f5d1
gabrielfmcoelho <gabrielcoelho09gc@gmail.com>
2022-12-22 17:16:30 -0300

    coelho

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,10 @@
 import './App.css'
 import NavBar from './components/NavBar'
+import Home from './components/Home'
 
 function App() {
   return (
-  
-    <div className='bg-primary w-full overflow-hidden'>
-      <div className=''></div>
-      
-      <NavBar />
-    </div>
+    <Home/>
   )
 }
 
---
+++ src/components/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <section className="bg-[url('./src/assets/logo.png')] bg-cover bg-no-repeat h-screen">
+        <div className='text-center'>
+            <h1 className="text-6xl font-bold text-black">Excelencia e qualidade</h1>
+        </div>
+    </section>
+  )
+}
+
+export default Home
